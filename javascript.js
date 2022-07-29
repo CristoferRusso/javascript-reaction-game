bd0f62777c75130ea481955c84423820c559d9d1
Cristofer <cristofer.russo94@gmail.com>
2022-07-29 10:12:00 +0200

    Update 29-07-22

--- javascript.js
+++ javascript.js
@@ -1,103 +1,112 @@
 //Funzione che risponde al click (scomparsa)
-document.addEventListener("DOMContentLoaded",function(){
-const shape = document.getElementById("shape");
+document.addEventListener("DOMContentLoaded", function () {
+    const shape = document.getElementById("shape");
 
-//Parte il cronometro
-let startTime = Date.now();
-let show = document.getElementById("time");
-let intId = null;
-let point = document.getElementById("score");
-let fast = document.getElementById("velocità");
+    //Parte il cronometro
+    let startTime = Date.now();
+    let show = document.getElementById("time");
+    let intId = null;
+    let point = document.getElementById("score");
+    let fast = document.getElementById("velocità");
 
-//Funzione alternativa con colori
-function randColor(){
-     const number = '0123456789ABCDEF'.split('');
-     let result ='#';
-     for(let i = 0; i<6; i++) {
-        result += number[Math.floor(Math.random()*16)];
-     }
-     return  result;
-}
+    //Funzione alternativa con colori
+    function randColor() {
+        //Si crea un array e si usa la funzione split che divide i valori all'interno dell'array tra apici 
+        const number = '0123456789ABCDEF'.split('');
+        let result = '#';
+        //Si inizializza un For che incrementa ogni volta fino ad arricare a 6 valori
+        for (let i = 0; i < 6; i++) {
 
-
-//Calola il tempo impiegato
-function elapsedTime(t1, t2){
-    const t =(t2-t1)/1000;
-    return t.toFixed(2).toLocaleString();
-}
-
-function elapsedScore(t1, t2){
-    const t = t2 + t3;
-    return t.toFixed(2).toLocaleString();
-}
-
-
-shape.addEventListener("click", shapeCliked);
-
-makeShapeAppear1();
-
-function makeShapeAppear1() {
-   //Pulisce setinterval
-    if(intId){
-        clearInterval(intId);
+            //Si utilizza la funzione random per la generazione casuale
+            result += number[Math.floor(Math.random() * 16)];
+        }
+        return result;
     }
- //Setinterval chiama una funzione specificata a intervalli specifi dichiarati in millisecondi   
- intId = setInterval(()=> {
- //Fa ricomparire l'immagine dopo un periodo casuale utilizzando il metodo di default dell'immagine
- shape.style.display="block";
-
- let w = Math.random()*200;
-     if(w <10) {
-        w = 10;
-     }
-
-     if(w > 200) {
-        w = 200;
-     }
-     
- shape.style.top = Math.random()*550 +"px";
- shape.style.left = Math.random()*1500 +"px";
- shape.style.height = w +"px";
- shape.style.width = w +"px";
- shape.style.backgroundColor = randColor();
 
 
+    //Calola il tempo impiegato
+    function elapsedTime(t1, t2) {
+        const t = (t2 - t1) / 1000;
+        return t.toFixed(2).toLocaleString();
+    }
+    //Calcola i punti (Da sistemare)
+    function elapsedScore(t1, t2) {
+        const t = t2 + t3;
+        return t.toFixed(2).toLocaleString();
+    }
 
-},800);
 
-}
+    shape.addEventListener("click", shapeCliked);
 
-//Alternativa a makeShapeAppear1 settimeout apetta il tempo specificato e poi finisce
-function makeShapeAppear2() {
-    //Pulisce il timeout
-     if(intId){
-         clearTimeout(intId);
-     }
-  intId = setTimeout(()=> {
-  //Fa ricomparire l'immagine dopo un periodo casuale
-  shape.style.display="block";
+    makeShapeAppear1();
 
-    
- }, Math.random()*2000);
+    function makeShapeAppear1() {
+        //Pulisce setinterval
+        if (intId) {
+            clearInterval(intId);
+        }
+        //Setinterval chiama una funzione specificata a intervalli specifi dichiarati in millisecondi   
+        intId = setInterval(() => {
+            //Fa ricomparire l'immagine dopo un periodo casuale utilizzando il metodo di default dell'immagine
+            shape.style.display = "block";
 
- 
- }
- 
+            let w = Math.random() * 200;
+            if (w < 10) {
+                w = 10;
+            }
 
-function shapeCliked(){
-   
-   //Fa scomparire l'immagine
-   shape.style.display="none";
+            if (w > 200) {
+                w = 200;
+            }
+            //Alterna i cerchi e i quadrati in maniera random
+            if (Math.random() > .5) {
+                shape.style.borderRadius = "50%";
 
-    // Scrive il tempo calcolato nella variabile time(html)
-   
-   show.innerHTML = (elapsedTime(startTime, Date.now()));
+            } else shape.style.borderRadius = "0%";
 
-   //Azzera il cronometro
-   startTime = Date.now();
+            shape.style.top = Math.random() * 550 + "px";
+            shape.style.left = Math.random() * 1500 + "px";
+            shape.style.height = w + "px";
+            shape.style.width = w + "px";
+            shape.style.backgroundColor = randColor();
 
-   document.getElementById("score").value = score + 1;
-}
+
+
+        }, 1000);
+
+    }
+
+    //Alternativa a makeShapeAppear1 settimeout apetta il tempo specificato e poi finisce
+    function makeShapeAppear2() {
+        //Pulisce il timeout
+        if (intId) {
+            clearTimeout(intId);
+        }
+        intId = setTimeout(() => {
+            //Fa ricomparire l'immagine dopo un periodo casuale
+            shape.style.display = "block";
+
+
+        }, Math.random() * 2000);
+
+
+    }
+
+
+    function shapeCliked() {
+
+        //Fa scomparire l'immagine
+        shape.style.display = "none";
+
+        // Scrive il tempo calcolato nella variabile time(html)
+
+        show.innerHTML = (elapsedTime(startTime, Date.now()));
+
+        //Azzera il cronometro
+        startTime = Date.now();
+
+        document.getElementById("score").value = score + 1;
+    }
 
 
 
